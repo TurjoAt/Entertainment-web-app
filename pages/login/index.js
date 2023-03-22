468c1fdf23433fbe0f5caed830e8e7016e9b439d
SurriyaLuavan <surriyaluavan@gmail.com>
2023-03-22 18:10:16 -0300

    added home link to login and sigin page

--- pages/login/index.js
+++ pages/login/index.js
@@ -32,17 +32,19 @@ const Login = () => {
         <link rel="icon" href="/assets/favicon-32x32.png" />
       </Head>
       <main className={styles.loginContainer}>
-        <svg
-          className={styles.logo}
-          width="33"
-          height="27"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="m26.463.408 3.2 6.4h-4.8l-3.2-6.4h-3.2l3.2 6.4h-4.8l-3.2-6.4h-3.2l3.2 6.4h-4.8l-3.2-6.4h-1.6a3.186 3.186 0 0 0-3.184 3.2l-.016 19.2a3.2 3.2 0 0 0 3.2 3.2h25.6a3.2 3.2 0 0 0 3.2-3.2V.408h-6.4Z"
-            fill="#FC4747"
-          />
-        </svg>
+        <Link href="/" data-tab="home">
+          <svg
+            className={styles.logo}
+            width="33"
+            height="27"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="m26.463.408 3.2 6.4h-4.8l-3.2-6.4h-3.2l3.2 6.4h-4.8l-3.2-6.4h-3.2l3.2 6.4h-4.8l-3.2-6.4h-1.6a3.186 3.186 0 0 0-3.184 3.2l-.016 19.2a3.2 3.2 0 0 0 3.2 3.2h25.6a3.2 3.2 0 0 0 3.2-3.2V.408h-6.4Z"
+              fill="#FC4747"
+            />
+          </svg>
+        </Link>
         <form
           className={`${styles.formContainer} bg-primary-600`}
           onSubmit={handleSubmit}
