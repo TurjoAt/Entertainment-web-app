392a3d681a1c1e89e0ebce073b9a54bc41af315d
SurriyaLuavan <surriyaluavan@gmail.com>
2023-03-22 18:19:04 -0300

    fixed logo centering

--- pages/login/index.js
+++ pages/login/index.js
@@ -32,13 +32,8 @@ const Login = () => {
         <link rel="icon" href="/assets/favicon-32x32.png" />
       </Head>
       <main className={styles.loginContainer}>
-        <Link href="/" data-tab="home">
-          <svg
-            className={styles.logo}
-            width="33"
-            height="27"
-            xmlns="http://www.w3.org/2000/svg"
-          >
+        <Link className={styles.logo} href="/" data-tab="home">
+          <svg width="33" height="27" xmlns="http://www.w3.org/2000/svg">
             <path
               d="m26.463.408 3.2 6.4h-4.8l-3.2-6.4h-3.2l3.2 6.4h-4.8l-3.2-6.4h-3.2l3.2 6.4h-4.8l-3.2-6.4h-1.6a3.186 3.186 0 0 0-3.184 3.2l-.016 19.2a3.2 3.2 0 0 0 3.2 3.2h25.6a3.2 3.2 0 0 0 3.2-3.2V.408h-6.4Z"
               fill="#FC4747"
